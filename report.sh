2ea31c34b90085a30ee8a6fa59046ea6593100b5
Maxwell <qing1.zhang@intel.com>
2021-06-19 12:47:43 +0800

    call notify if available after report generated

--- report.sh
+++ report.sh
@@ -2,3 +2,6 @@
 CUR_DIR=$(cd `dirname ${BASH_SOURCE[0]}`; pwd)
 LOG_DIR=/var/log/monitor
 python3 $CUR_DIR/report.py > $LOG_DIR/report.txt
+if [ -f ${CUR_DIR}/notify.sh ]; then
+    ${CUR_DIR}/notify.sh
+fi
