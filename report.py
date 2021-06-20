989d656dc958eaf2cdb1708ea30b73b5aa075a15
Maxwell <qing1.zhang@intel.com>
2021-06-20 12:53:40 +0800

    change data range to 1 day

--- report.py
+++ report.py
@@ -369,7 +369,7 @@ def report(file_name):
     color_list = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
     
     now = DT.datetime.now()
-    delta = DT.timedelta(days=2)
+    delta = DT.timedelta(days=1)
     day_before = now - delta
     print('Sys report from {} to {}'.format(str(day_before), str(now)))
     try:
