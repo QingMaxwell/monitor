4e1a677470fb426989a1450e5d2e938fde478d4b
Maxwell <qing1.zhang@intel.com>
2021-06-21 09:51:06 +0000

    add smartmontools install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /var/log/monitor
-sudo apt install -y lm-sensors hddtemp nvme-cli sysstat python3-pip
+sudo apt install -y lm-sensors hddtemp nvme-cli sysstat python3-pip smartmontools
 sudo -H pip3 install numpy matplotlib
 
 # TODO add 0 8 * * * /opt/monitor/report.sh into /var/spool/cron/root
